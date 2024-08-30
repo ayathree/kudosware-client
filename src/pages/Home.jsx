f910866bbbf5fa66b6c0ac2c3aeb406d15b0b046
ayathree <nobanitaayathree333@gmail.com>
2024-08-30 17:09:45 +0600

    make home page  responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,17 +8,17 @@ const Home = () => {
     return (
         <div >
             {/* div 1 */}
-            <div className="hero bg-base-200  border-b-8 border-red-500 rounded-bl-full">
+            <div className="hero lg:bg-base-200  border-b-8 lg:border-red-500 lg:rounded-bl-full">
   <div className="hero-content  ">
    
-        <div className='flex flex-row justify-center items-center'>
-        <div className='ml-44'>
-        <h1 className='font-bold text-6xl'>Find Your Favorite 
+        <div className='flex lg:flex-row flex-col justify-center items-center'>
+        <div className='lg:ml-44'>
+        <h1 className='font-bold lg:text-6xl text-3xl'>Find Your Favorite 
             <br />
-            <span className='text-red-500 text-4xl'>Job Immediate</span></h1>
-            <p className='text-xl font-semibold mt-5 w-1/2  text-gray-500'>Find here your favorite job by searching, fill up your resume and find out the best job which is suitable for you.</p>
+            <span className='text-red-500 lg:text-4xl text-2xl'>Job Immediate</span></h1>
+            <p className='text-xl font-semibold mt-5 lg:w-1/2 w-full  text-gray-500'>Find here your favorite job by searching, fill up your resume and find out the best job which is suitable for you.</p>
         </div>
-        <img className='h-[420px] ' src={banner} alt="" />
+        <img className='h-[420px] hidden lg:block ' src={banner} alt="" />
         </div>
         
 
@@ -28,7 +28,7 @@ const Home = () => {
   </div>
 </div>
 {/* div 2 */}
-        <div className='flex flex-row justify-start items-center gap-5 mt-36'>
+        <div className='flex lg:flex-row flex-col lg:justify-start justify-center items-center lg:gap-5 mt-36'>
             <div>
                 <img className='w-[740px]' src={woman} alt="" />
 
@@ -37,12 +37,12 @@ const Home = () => {
                 <h1 className='font-bold text-5xl'>Find Million Of Jobs And
                     <br />
                 <span className='text-red-500 text-4xl'>Achieve Success</span></h1>
-                <p className='font-semibold mt-4 w-3/4'>Job portal is the one of the best job searching site, where you can find out millions of job and also can find the best match job for you. So what are you waiting for? Find out your best match job now</p>
+                <p className='font-semibold mt-4 lg:w-3/4  '>Job portal is the one of the best job searching site, where you can find out millions of job and also can find the best match job for you. So what are you waiting for? Find out your best match job now</p>
             </div>
         </div>
         {/* div 3 */}
-        <h1 className='text-5xl font-bold text-center mt-36'>Our Job <span className='text-red-500'>Process</span></h1>
-        <div className='flex flex-row justify-center items-center my-16'>
+        <h1 className='md:text-5xl text-3xl  font-bold text-center mt-36'>Our Job <span className='text-red-500'>Process</span></h1>
+        <div className='flex lg:flex-row flex-col justify-center items-center my-16'>
             <div className='border border-white w-full h-[220px] p-10 hover:shadow-2xl'>
            <div className='flex justify-center items-center'>
            <IoIosPeople className='text-4xl' />
