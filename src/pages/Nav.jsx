7ab137a8510fe023f8b2db9f3c73cc5855a45417
ayathree <nobanitaayathree333@gmail.com>
2024-08-30 14:08:02 +0600

    make navbar responsive

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
             <h1 className="font-bold text-3xl">Job <span className="text-red-500">Portal</span></h1>
 
             
-            <div className="flex lg:hidden">
+            <div className="flex md:hidden">
                 <button onClick={()=>setIsOpen(!isOpen)} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
                     {
                         isOpen ? (
@@ -38,17 +38,17 @@ const Nav = () => {
             </div>
         </div>
 
-        <div className={`${isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full'}  absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center`}>
+        <div className={`${isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full'}  absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-red-200 dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center`}>
             <div className="flex flex-col md:flex-row md:mx-6">
                 <Link to={'/'}><a className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Home</a></Link>
-                <Link to={'/resume'}><button className="ml-5 flex justify-center items-center hover:text-blue-500 dark:hover:text-blue-400 ">
+                <Link to={'/resume'}><button className=" md:ml-5 flex justify-center items-center hover:text-blue-500 dark:hover:text-blue-400 ">
                 <FaUpload  />
                 <a className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0" href="#">Upload Resume</a>
                 </button></Link>
             </div>
 
-            <div className="flex justify-center md:block">
-                <a className="relative text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="#">
+            <div className="flex md:justify-center md:block">
+                <a className=" ">
                    <Link to={'/signUp'}> <button className="bg-red-500 p-2 text-white rounded-md ">Sing Up</button></Link>
                 </a>
             </div>
