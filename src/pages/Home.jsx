615b8481ac3151bf0507ec7e1f5e9c8a2622acad
ayathree <nobanitaayathree333@gmail.com>
2024-08-30 13:34:51 +0600

    create the home third part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
+import { IoIosPeople } from 'react-icons/io';
 import banner from '../assets/image/pbanner.png'
 import woman from '../assets/image/women.png'
+import { FaSearch } from 'react-icons/fa';
+import { IoDocumentText } from 'react-icons/io5';
 
 const Home = () => {
     return (
@@ -37,7 +40,37 @@ const Home = () => {
                 <p className='font-semibold mt-4 w-3/4'>Job portal is the one of the best job searching site, where you can find out millions of job and also can find the best match job for you. So what are you waiting for? Find out your best match job now</p>
             </div>
         </div>
-        
+        {/* div 3 */}
+        <h1 className='text-5xl font-bold text-center mt-36'>Our Job <span className='text-red-500'>Process</span></h1>
+        <div className='flex flex-row justify-center items-center mt-16'>
+            <div className='border border-white w-full h-[220px] p-10 hover:shadow-2xl'>
+           <div className='flex justify-center items-center'>
+           <IoIosPeople className='text-4xl' />
+           </div>
+            <h1 className='text-red-500 text-2xl font-bold text-center'>Create Account</h1>
+            <p className='text-center'>Create an account for you for get the access of job search</p>
+
+            </div>
+            <div className='border border-white w-full h-[220px] p-10 hover:shadow-2xl'>
+           <div className='flex justify-center items-center'>
+           <FaSearch className='text-3xl' />
+           </div>
+            <h1 className='text-red-500 text-2xl font-bold text-center'>Search Job</h1>
+            <p className='text-center'>Search job according to your profile.</p>
+           
+
+            </div>
+            <div className='border border-white w-full h-[220px] p-10 hover:shadow-2xl'>
+           <div className='flex justify-center items-center'>
+           <IoDocumentText className='text-3xl' />
+           </div>
+            <h1 className='text-red-500 text-2xl font-bold text-center'>Upload Resume</h1>
+            <p className='text-center'>Upload your resume for recruiter to know your profile well.</p>
+
+            </div>
+            
+
+        </div>
         </div>
     );
 };
