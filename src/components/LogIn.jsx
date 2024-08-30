ee48a8f127741d8a7511c79734d2653a3adf6c79
ayathree <nobanitaayathree333@gmail.com>
2024-08-30 16:55:56 +0600

    make login page  responsive

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -10,7 +10,7 @@ const LogIn = () => {
             <h1 className="text-5xl font-bold">Your dream Job
                 <br />
                  <span className="text-3xl text-red-500" >Is Near to You</span></h1>
-            <img src={banner} alt="" />
+            <img className="hidden lg:block" src={banner} alt="" />
             </div>
 {/* form */}
             <div className='mt-20 mb-5 text-center text-4xl font-bold'>
@@ -18,25 +18,25 @@ const LogIn = () => {
             </div>
             <div className='flex justify-center items-center mb-5'>
 
-    <input type="email" placeholder='Your Email' className="mt-2 rounded-lg border w-1/2 border-black bg-white px-5 py-2.5  focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" />
+    <input type="email" placeholder='Your Email' className="mt-2 rounded-lg border md:w-1/2 w-full border-black bg-white px-5 py-2.5  focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" />
     
 </div>
 
 <div className='flex justify-center items-center mb-5'>
 
-    <input type="password" placeholder='Your Password' className="mt-2 rounded-lg border w-1/2 border-black bg-white px-5 py-2.5  focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" />
+    <input type="password" placeholder='Your Password' className="mt-2 rounded-lg border md:w-1/2 w-full border-black bg-white px-5 py-2.5  focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40" />
     
 </div>
 
 
 <div className='flex justify-center items-center mb-5'>
 
-    <button  className="mt-2 rounded-lg border w-1/2  bg-red-500 text-white px-5 py-2.5 hover:bg-black">Log In</button>
+    <button  className="mt-2 rounded-lg border md:w-1/2 w-full  bg-red-500 text-white px-5 py-2.5 hover:bg-black">Log In</button>
     
 </div>
 
 {/* login */}
-<div className='m-20 text-center'>
+<div className='md:m-20 m-10 text-center'>
   <h1>Do Not Have An Account? <Link to={'/signUp'}><span className='font-bold'>Sign Up here</span></Link></h1>
 
 </div>
